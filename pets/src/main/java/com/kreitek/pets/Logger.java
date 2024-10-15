bba77daabe7b663fdd6d8e5b811d565e1238f68d
Angel Hernandez <angelillohdez@gmail.com>
2024-10-15 13:49:17 +0200

    Logger logic implemented & fixed a typo

---
+++ pets/src/main/java/com/kreitek/pets/Logger.java
@@ -0,0 +1,23 @@
+package com.kreitek.pets;
+
+public class Logger {
+
+    private static Logger instance;
+
+    private static int contador;
+
+    private Logger() {}
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+
+    }
+
+    public void debug(String message) {
+        contador++;
+        System.out.println("[debug][" + contador + "] - " + message);
+    }
+}
--- pets/src/main/java/com/kreitek/pets/PetApp.java
+++ pets/src/main/java/com/kreitek/pets/PetApp.java
@@ -7,12 +7,13 @@ import java.util.Scanner;
 
 public class PetApp {
 
-    // TODO Logger declaration
+    private final Logger logger = Logger.getInstance();
 
     public static void main (String[] args) {
+        PetApp app = new PetApp();
         ControllerFactory controllerFactory = new ControllerFactory();
         boolean end = false;
-        System.out.println("Pet app has been initiated"); // TODO Logger
+        app.logger.debug("Pet app has been initiated");
         while (!end) {
             String command = waitForNewCommand();
             String[] commandArgs = command.split(":");
@@ -58,7 +59,7 @@ public class PetApp {
                 System.out.println("Bad command error");
             }
         }
-        System.out.println("Pet app has been ended"); // TODO Logger
+        app.logger.debug("Pet app has been ended");
     }
 
     private static String getGetParam(String[] commandArgs) throws BadCommandException {
